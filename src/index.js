02fbde5f2b9126553c196a691aae1cc67a33dedb
Brent <karmapoint@gmail.com>
2017-09-21 11:19:43 -0700

    add api call to app

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-// React library has split...
-// React is used to create components
-import React from 'react';
-
-// ReactDOM is used to render them on the page
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyANT8W-S5EqhGRi9rmvtIBI5gxcoZd_w8M';
 
 
 
-// STEP 1: Create new component that produces HTML
-const App = function (){
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
-// or can be rewritten as 1 line arrow function
-// const App = () => (<div>Hi!</div>);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// STEP 2: Insert component's html into the DOM
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'kittens'}, (videos) => {
+      this.setState({ videos });
+      // same as this.setState({ videos: videos }) = ES6 shortcut
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-// render has 2 arguments... first a jsx tag of the component
-// if we just did App instead of <App />, it is referencing the
-// factory function.  Second is where the component should be rendered.
